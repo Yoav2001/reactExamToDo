766b94e0fa7bddcfe24cce4d2132b895ff998ae8
yoav2001 <shalev0125@gmail.com>
2022-01-02 16:48:08 +0200

    complete modal of update task

---
+++ front/src/server/middleware/interceptor.ts
@@ -0,0 +1,16 @@
+
+export const defaultError =function (error:any){
+
+    const {config,data,status} =error.response
+    const {method,baseURL,url}=config
+
+    if(error.response){
+        console.log(`client has recieved an error  on a ${method}  request  on the url ${baseURL}${url}`);
+
+        
+
+    }
+
+}
+
+export default defaultError;
