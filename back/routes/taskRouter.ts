eded064d864d53d9b596f59b8a77caddded1245a
yoav2001 <shalev0125@gmail.com>
2022-01-09 16:08:25 +0200

    add controoler task

--- back/routes/taskRouter.ts
+++ back/routes/taskRouter.ts
@@ -11,61 +11,13 @@ import taskController  from '../controllers/taskController'
 const router = express.Router();
 //task/addTask
 router.post("/addTask",taskController.addNewTask) 
-// => {
-    // const {
-    //   emailUserOfTask,
-    //   taskName,
-    //   startDate,
-    //   endTime,
-    //   isComplete,
-    //   isRelevent,
-    // }: taskModal.Task = req.body;
-    // const user = res.locals.user as usersModel.User;
 
-    // if (emailUserOfTask !== user.email && !user.isAdmin) {
-    //   const errorObj: ErrorHandlerType = {
-    //     statusError: 401,
-    //     errorMap: errorHandler.errorMapToDoApp,
-    //     uniqueMessage: "opps you try to add task to other user ",
-    //   };
-    //   return next(errorObj);
-    // }
-    // if (
-    //   !emailUserOfTask||
-    //   !taskName||
-    //   !startDate ||
-    //   isComplete === undefined ||
-    //   isRelevent === undefined
-    // ) {
-    //   const errorObj: ErrorHandlerType = {
-    //     statusError: 400,
-    //     errorMap: errorHandler.errorMapToDoApp,
-    //   };
-    //   return next(errorObj);
-    // }
-    // const taskToAdd: taskModal.Task = {
-    //   emailUserOfTask: emailUserOfTask,
-    //   taskName: taskName,
-    //   startDate: startDate,
-    //   endTime: endTime,
-    //   isComplete: isComplete,
-    //   isRelevent: isRelevent,
-    // };
-    // const data = await taskService.addNewTask(taskToAdd);
-
-
-    // res.status(200).json({ key: data });
-    
-    
-//   }
-// );
 
 //task/
 router.get("/", auth.adminMiddleware,async (  req: express.Request,  res: express.Response,  next: express.NextFunction ) => {
     const user = res.locals.user as usersModel.User;
-    const arrAllTasks: taskModal.Task[] | undefined =
-      await taskService.getAllTasks();
-    res.json(arrAllTasks);
+  
+  
   }
 );
 
